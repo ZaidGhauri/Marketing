e3794acb27cfbcc83d1988350a72192658912362
Zainab <zainiir0311@gmail.com>
2016-12-25 01:14:25 +0500

    autocomplete

--- Marketing/Controllers/SearchController.cs
+++ Marketing/Controllers/SearchController.cs
@@ -9,46 +9,51 @@ namespace Marketing.Controllers
 {
     public class SearchController : Controller
     {
-        // GET: /Search/
-
-        public ActionResult SearchIndex()
-        {
-            return View();
-        }
         [AcceptVerbs(HttpVerbs.Post)]
         public JsonResult Autocomplete(string term)
         {
             var result = new List<KeyValuePair<string, string>>();
+
             IList<SelectListItem> List = new List<SelectListItem>();
             List.Add(new SelectListItem { Text = "test1", Value = "0" });
             List.Add(new SelectListItem { Text = "test2", Value = "1" });
             List.Add(new SelectListItem { Text = "test3", Value = "2" });
             List.Add(new SelectListItem { Text = "test4", Value = "3" });
+
             foreach (var item in List)
             {
+
                 result.Add(new KeyValuePair<string, string>(item.Value.ToString(), item.Text));
+
+
             }
             var result3 = result.Where(s => s.Value.ToLower().Contains(term.ToLower())).Select(w => w).ToList();
             return Json(result3, JsonRequestBehavior.AllowGet);
         }
+
         [AcceptVerbs(HttpVerbs.Post)]
-        public JsonResult GetDetail(int id)
+        public JsonResult Details(int id)
         {
-            DemoModel model = new DemoModel();
+           modelx model = new modelx();
             // select data by id here display static data;
             if (id == 0)
             {
                 model.id = 1;
                 model.name = "Yogesh Tyagi";
                 model.mobile = "9460516787";
+
             }
             else
             {
                 model.id = 2;
                 model.name = "Pratham Tyagi";
                 model.mobile = "9460516787";
+
             }
+
             return Json(model);
+
         }
+
     }
 }
